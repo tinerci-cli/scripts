2d64c938ea2443dc2cc97bf48700012e44db14f9
tinerci <yarame@freso.com>
2022-12-01 14:16:28 +0100

    small changes

--- login_kb.sh
+++ login_kb.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# change functionality of capslock and esc
-# make a single press on super open menu, cause xfce is a little autistic
-# activate numlock on login
+#change functionality of capslock and esc
 /bin/setxkbmap -option "caps:swapescape"
+#make a single press on super open menu, cause xfce is a little autistic
 /bin/xcape -e 'Super_L=Alt_L|F1'
+#activate numlock on login
 /bin/numlockx
+#decrease key repeat dealay to 300ms, increase repeat rate to 50 per second
+/bin/xset r rate 300 50
