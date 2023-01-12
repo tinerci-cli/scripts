1e6b62177723876feb835c523c08e6ee59dd8400
tinerci <yarame@freso.com>
2023-01-12 16:50:24 +0100

    adjustments

--- startup_programs.sh
+++ startup_programs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 keepassxc &
 liferea &
-sleep 1; #let the windows hit the screen
-kp_id="$(wmctrl -l | grep KeePass | awk '{print $1}')" #window ID of keepass
+sleep 3; #let the windows hit the screen
 lr_id="$(wmctrl -l | grep Liferea | awk '{print $1}')" #window ID of keepass
-wmctrl -i -r $kp_id -t 3 && wmctrl -i -r $kp_id -b toggle,shaded
 wmctrl -i -r $lr_id -b toggle,hidden
+kp_id="$(wmctrl -l | grep KeePass | awk '{print $1}')" #window ID of keepass
+wmctrl -i -r $kp_id -t 3 && wmctrl -i -r $kp_id -b toggle,hidden
