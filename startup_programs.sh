7f826a8ca73d27beeee10e28a7d64e656a374735
name <gnuraver@freetard.org>
2023-05-10 10:25:49 +0200

    smoll

--- startup_programs.sh
+++ startup_programs.sh
@@ -5,4 +5,4 @@ sleep 3; #let the windows hit the screen
 lr_id="$(wmctrl -l | grep Liferea | awk '{print $1}')" #window ID of keepass
 wmctrl -i -r $lr_id -b toggle,hidden
 kp_id="$(wmctrl -l | grep KeePass | awk '{print $1}')" #window ID of keepass
-wmctrl -i -r $kp_id -t 3 && wmctrl -i -r $kp_id -b toggle,hidden
+wmctrl -i -r $kp_id -t 4 && wmctrl -i -r $kp_id -b toggle,hidden
