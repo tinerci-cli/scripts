26c4bf05b6476759c872ecbdb7734e161f31bdfe
name <gnuraver@freetard.org>
2023-06-30 22:19:24 +0200

    update script

--- login_kb.sh
+++ login_kb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #change functionality of capslock and esc
-/bin/setxkbmap -option "caps:swapescape"
+# /bin/setxkbmap -option "caps:swapescape" # changed in layout config /usr/share/X11/xkb/symbols/{de,us}
 #make a single press on super open menu, cause xfce is a little autistic
 /bin/xcape -e 'Super_L=Alt_L|F1'
 #activate numlock on login
