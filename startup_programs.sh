6ec6dbe55fddcfd9a70b1df19c3a8bfe536154d1
mete <mete@localhost.localdomain>
2023-12-24 16:41:09 +0100

    minimal

--- startup_programs.sh
+++ startup_programs.sh
@@ -6,3 +6,5 @@ lr_id="$(wmctrl -l | grep Liferea | awk '{print $1}')" #window ID of keepass
 wmctrl -i -r $lr_id -b toggle,hidden
 kp_id="$(wmctrl -l | grep KeePass | awk '{print $1}')" #window ID of keepass
 wmctrl -i -r $kp_id -t 4 && wmctrl -i -r $kp_id -b toggle,hidden
+fc_id="$(wmctrl -l | grep FCast | awk '{print $1}')" #window ID of keepass
+wmctrl -ic $fc_id
