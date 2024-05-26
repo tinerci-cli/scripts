27255cc584c9822116a7987e4e8af093d92d8a2a
mete <mete@localhost.localdomain>
2024-05-26 17:28:47 +0200

    newst

--- startup_programs.sh
+++ startup_programs.sh
@@ -8,3 +8,4 @@ kp_id="$(wmctrl -l | grep KeePass | awk '{print $1}')" #window ID of keepass
 wmctrl -i -r $kp_id -t 4 && wmctrl -i -r $kp_id -b toggle,hidden
 fc_id="$(wmctrl -l | grep FCast | awk '{print $1}')" #window ID of keepass
 wmctrl -ic $fc_id
+piper & killall piper
