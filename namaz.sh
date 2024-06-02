517b8f312a26e744e27a24a8be36732023e28e27
mete <mete@localhost.localdomain>
2024-06-03 01:45:57 +0200

    date bug

--- namaz.sh
+++ namaz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 text(){
 	names=("Fajr" "Sonnenaufgang" "Dhuhr" "Asr" "Maghrib" "Isha")
-	link="https://www.muwaqqit.com/index?lt=50.0782184&ln=8.2397608&d=2024-5-20&tztype=auto&tz=Europe%2FBerlin&vc=5.65&diptype=apparent&eo=121&eh=121&ehtype=observer&t=15.0&p=1010.0&k=0.155&zt=1.0&fa=-12.0&era=-16.0&ea=-12.0&ia=4.5&isn=-10.0&fea=1.0&rsa=1.0&isna=1.0&z=1&q=&add=Luisenpl.+4%2C+65185+Wiesbaden%2C+Germany"
+	link="https://www.muwaqqit.com/index?lt=50.0782184&ln=8.2397608&d=$(date -I)&tztype=auto&tz=Europe%2FBerlin&vc=5.65&diptype=apparent&eo=121&eh=121&ehtype=observer&t=15.0&p=1010.0&k=0.155&zt=1.0&fa=-12.0&era=-16.0&ea=-12.0&ia=4.5&isn=-10.0&fea=1.0&rsa=1.0&isna=1.0&z=1&q=&add=Luisenpl.+4%2C+65185+Wiesbaden%2C+Germany"
 	times=($(curl "$link" -s |
 		grep -P "\d\d:\d\d:\d\d" |
 		sed -n '2p;4p;11p;12p;15p;21p' |
