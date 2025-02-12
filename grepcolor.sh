d15b000d2e0e9ed68655e81fdc9b5c5f43b11490
Your Name <you@example.com>
2025-02-12 17:25:30 +0100

    too much

--- grepcolor.sh
+++ grepcolor.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 eval "$(xdotool getmouselocation --shell)" #get position of mouse
 IMAGE="$(import -window root -depth 8 -crop 1x1+"$X"+"$Y" txt:-)" #save current window, crop the pixels to where the mouse is
 COLOR="$(echo "$IMAGE" | grep -om1 '#\w\+')" #grep the color
